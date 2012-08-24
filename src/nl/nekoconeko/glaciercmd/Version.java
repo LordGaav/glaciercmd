7c7c5a8860ef56c3d2a974ce722c766a33195fd1
Nick Douma <n.douma@nekoconeko.nl>
2012-08-24 20:58:55 +0200

    Add Version metadata

---
+++ src/nl/nekoconeko/glaciercmd/Version.java
@@ -0,0 +1,16 @@
+package nl.nekoconeko.glaciercmd;
+
+public final class Version {
+	/**
+	 * Official name of the project
+	 */
+	public static final String PROJECT_NAME = "glaciercmd";
+	/**
+	 * Official release version of the project
+	 */
+	public static final String RELEASE_VERSION = "0.1";
+	/**
+	 * Actual Git revision according to `git describe` during bumping
+	 */
+	public static final String BUILD_VERSION = "";
+}
