160565faf27528ab374c1bcf3d6821419679f42d
Nick Douma <n.douma@nekoconeko.nl>
2012-08-24 22:52:51 +0200

    Add UPLOAD and DOWNLOAD to ConfigModes

--- src/nl/nekoconeko/glaciercmd/config/ConfigModes.java
+++ src/nl/nekoconeko/glaciercmd/config/ConfigModes.java
@@ -29,11 +29,24 @@ public class ConfigModes {
 		helpmode.setOptionalArg(true);
 		ConfigParameter versionmode = new ConfigParameter("v", "version", false, "Show version information");
 		ConfigParameter listmode = new ConfigParameter("l", "list", true, "TYPE", "List Glacier objects (vault|archive)");
+		ConfigParameter uploadmode = new ConfigParameter("u", "upload", false, "Upload file into a vault");
+		ConfigParameter downloadmode = new ConfigParameter("d", "download", false, "Download file from a vault. Note: this command will block until the file has been downloaded, and will probably take multiple hours.");
+
+		// Selectors
+		ConfigParameter vault = new ConfigParameter("vault", true, "VAULT", "Select this vault");
+		ConfigParameter archive = new ConfigParameter("archive", true, "ARCHIVE", "Use this archive id");
+
+		// User input
+		ConfigParameter inputfile = new ConfigParameter("input", true, "INPUT", "Use this file as input");
+		ConfigParameter outputfile = new ConfigParameter("output", true, "OUTPUT", "Use this file as output");
+		ConfigParameter description = new ConfigParameter("description", true, "DESC", "Use this description");
 
 		OptionGroup modes = new OptionGroup();
 		modes.addOption(listmode);
 		modes.addOption(helpmode);
 		modes.addOption(versionmode);
+		modes.addOption(uploadmode);
+		modes.addOption(downloadmode);
 		modes.setRequired(true);
 
 		// Options for root
@@ -52,6 +65,20 @@ public class ConfigModes {
 		list.addOption(secret);
 		list.addOption(config);
 
+		// Options for upload
+		ConfigMode upload = new ConfigMode();
+		upload.addRequiredOption(uploadmode);
+		upload.addRequiredOption(vault);
+		upload.addRequiredOption(inputfile);
+		upload.addRequiredOption(description);
+
+		// Options for download
+		ConfigMode download = new ConfigMode();
+		download.addRequiredOption(downloadmode);
+		download.addRequiredOption(vault);
+		download.addRequiredOption(archive);
+		download.addRequiredOption(outputfile);
+
 		// Options for help
 		ConfigMode help = new ConfigMode();
 		help.addRequiredOption(helpmode);
@@ -64,6 +91,8 @@ public class ConfigModes {
 		ConfigModes.addMode(ModeType.HELP, help);
 		ConfigModes.addMode(ModeType.VERSION, version);
 		ConfigModes.addMode(ModeType.LIST, list);
+		ConfigModes.addMode(ModeType.UPLOAD, upload);
+		ConfigModes.addMode(ModeType.DOWNLOAD, download);
 	}
 
 	private static void addMode(ModeType type, ConfigMode mode) {
