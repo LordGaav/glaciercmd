b613de5c57217c64cca7f96a870e462c594fbced
Nick Douma <n.douma@nekoconeko.nl>
2012-08-25 00:24:12 +0200

    Add initiateRetrieveVaultInventory and retrieveVaultInventoryJob to GlacierClient

--- src/nl/nekoconeko/glaciercmd/GlacierClient.java
+++ src/nl/nekoconeko/glaciercmd/GlacierClient.java
@@ -12,6 +12,11 @@ import com.amazonaws.services.glacier.AmazonGlacierClient;
 import com.amazonaws.services.glacier.model.DescribeVaultOutput;
 import com.amazonaws.services.glacier.model.DescribeVaultRequest;
 import com.amazonaws.services.glacier.model.DescribeVaultResult;
+import com.amazonaws.services.glacier.model.GetJobOutputRequest;
+import com.amazonaws.services.glacier.model.GetJobOutputResult;
+import com.amazonaws.services.glacier.model.InitiateJobRequest;
+import com.amazonaws.services.glacier.model.InitiateJobResult;
+import com.amazonaws.services.glacier.model.JobParameters;
 import com.amazonaws.services.glacier.model.ListVaultsRequest;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 import com.amazonaws.services.glacier.transfer.UploadResult;
@@ -67,6 +72,62 @@ public class GlacierClient {
 		} catch (AmazonClientException e) {
 			System.err.println("An error occured while executing the request: " + e.getLocalizedMessage());
 			System.exit(1);
+		}
+	}
+
+	protected String initiateRetrieveVaultInventory(String vault) {
+		InitiateJobRequest initJobRequest = new InitiateJobRequest();
+		initJobRequest.setVaultName(vault);
+		initJobRequest.setJobParameters(new JobParameters().withType("inventory-retrieval"));
+
+		InitiateJobResult initJobResult = this.client.initiateJob(initJobRequest);
+		return initJobResult.getJobId();
+	}
+
+	protected String retrieveVaultInventoryJob(String vault, String jobId) throws InterruptedException {
+		GetJobOutputRequest job = new GetJobOutputRequest();
+		job.setVaultName(vault);
+		job.setJobId(jobId);
+
+		GetJobOutputResult res = null;
+		boolean stop = false;
+		while (!stop) {
+			int code = 0;
+			String error = "";
+			try {
+				res = this.client.getJobOutput(job);
+				code = res.getStatus();
+			} catch (AmazonServiceException e) {
+				error = e.getMessage();
+				code = e.getStatusCode();
+			}
+
+			switch (code) {
+			case 200:
+				stop = true;
+				break;
+			case 400:
+				if (error.contains("InProgress")) {
+					Formatter.printInfoLine("Job is not finished yet, waiting...");
+				} else {
+					Formatter.printErrorLine("AWS returned code 400. Body Malformed.");
+					System.exit(1);
+				}
+				break;
+			case 404:
+				Formatter.printErrorLine("AWS returned code 404. Vault or Job could not be found.");
+				System.exit(1);
+			default:
+				Formatter.printInfoLine(String.format("Job returned code %d, waiting 30 seconds", code));
+				break;
+			}
+			Thread.sleep(1000 * 60);
+		}
+
+		try {
+			return new java.util.Scanner(res.getBody()).useDelimiter("\\A").next();
+		} catch (java.util.NoSuchElementException e) {
+			return "";
 		}
 	}
 }
